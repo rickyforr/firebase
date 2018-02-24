bd031d17a2f137af563d0c76bc50066713077885
Richard Forrester <richard.forrester@gmail.com>
2018-02-23 23:24:06 -0700

    added js files for page components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from './Navigation';
+import LandingPage from './Landing';
+import SignUpPage from './SignUp';
+import SignInPage from './SignIn';
+import PasswordForgetPage from './PasswordForget';
+import HomePage from './Home';
+import AccountPage from './Account';
+
+import * as routes from '../constants/routes';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-          <Navigation />
-      </Router>
+        <Router>
+          <div>
+            <Navigation />
+            <hr/>
+            <Route
+                exact path={routes.LANDING}
+                component={() => <LandingPage />}
+            />
+            <Route
+                exact path={routes.SIGN_UP}
+                component={() => <SignUpPage />}
+            />
+            <Route
+                exact path={routes.SIGN_IN}
+                component={() => <SignInPage />}
+            />
+            <Route
+                exact path={routes.PASSWORD_FORGET}
+                component={() => <PasswordForgetPage />}
+            />
+            <Route
+                exact path={routes.HOME}
+                React Router and Routes
+            component={() => <HomePage />}
+          />
+          <Route
+              exact path={routes.ACCOUNT}
+              component={() => <AccountPage />}
+          />
+        </div>
+        </Router>
     );
   }
 }
